dbb5cca1bff9cdcde3725368091a1ee3bfe42866
Emmanuel Zoe <emanuellzoee@gmail.com>
2026-03-25 01:56:48 +0700

    fix: update package.json to specify node engine version and remove node-fetch dependency

--- api-middleware/src/index.ts
+++ api-middleware/src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import fetch from "node-fetch";
 import fallbackProducts from "./data/products.json";
 
 const app = express();
